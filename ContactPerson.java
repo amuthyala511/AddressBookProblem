57356cb7879147b6af33e72c94c48ac07a37b2f8
Aishwarya <amuthyala511@gmail.com>
2020-10-23 16:03:39 +0530

    Added new person details to address book

--- ContactPerson.java
+++ ContactPerson.java
@@ -5,17 +5,73 @@ public class ContactPerson
 	public String lastName;
 	public String address;
 	public String city;
-	public  String state;
-	public long phoneNumber;
-	public long zip;
-	public ContactPerson(String firstName, String lastName,String address,String city,String state,long phoneNumber,long zip)
+	public String state;
+	public int zip;
+	public int phoneNumber;
+	public ContactPerson(String firstName, String lastName,String address,String city,String state,int zip,int phoneNumber)
 	{
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.address = address;
 		this.city = city;
 		this.state = state;
-		this.phoneNumber = phoneNumber;
 		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+	}
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	public void setFirstName(String firstName)
+	{
+		this.firstName = firstName;
+	}
+	public String getLastName()
+	{
+		return lastName;
+	}
+	public void setLastName(String lastName)
+	{
+		this.lastName = lastName;
+	}
+	public String getAddress()
+	{
+		return address;
+	}
+	public void setAddress(String address)
+	{
+		this.address = address;
+	}
+	public String getCity()
+	{
+		return city;
+	}
+	public void setCity(String city)
+	{
+		this.city = city;
+	}
+	public String getState()
+	{
+		return state;
+	}
+	public void setState(String state)
+	{
+		this.state = state;
+	}
+	public int getZip()
+	{
+		return zip;
+	}
+	public void setZip(int zip)
+	{
+		this.zip = zip;
+	}
+	public int getPhoneNumber()
+	{
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber)
+	{
+		this.phoneNumber = phoneNumber;
 	}
 }
